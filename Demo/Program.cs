bb4070ca2a6cc924550aec6db90c203910334ed8
Egor Ushakov <eushakov@gmail.com>
2021-04-21 10:45:31 +0300

    Remove unused code

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,20 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Threading;
 using AreaCalculator;
 
 namespace Demo
 {
     internal class Program
     {
-        private static string StringifyParameterInfo(ParameterInfo info)
-        {
-            return $"Name: {info.Name}, Type: {info.ParameterType.Name}";
-        }
-
         private static void Main(string[] args)
         {
             var shapes = Calculator.GetAvailableShapes();
@@ -33,12 +26,10 @@ namespace Demo
             var ctorInfos = chosenShape.ConstructorInfos;
             Console.WriteLine($"You choose {shapeName}");
             Console.WriteLine($"There are {ctorInfos.Length} variant(s) of parameters of the {shapeName}");
-            // var ctorArgs = DisplayAndSelectConstructorArguments(ctorInfos);
             var ctorInfo = ChooseConstructor(ctorInfos);
             var ctorArgs = BuildConstructorArguments(ctorInfo);
             var s = ctorInfo.Invoke(ctorArgs);
             var shape = (IShape) s;
-            // InitializeShapeWithArguments(chosenShape.FullName, ctorArgs);
             Console.WriteLine("Congratulations!!!");
             Console.WriteLine($"The area of the {shapeName} equals to {Calculator.Area(shape)}");
         }
@@ -51,16 +42,12 @@ namespace Demo
                 var paramType = param.ParameterType;
                 Console.WriteLine($"Enter the value of type '{paramType}'");
                 var entered = Console.ReadLine();
-                if (null == entered)
-                {
-                    throw new ArgumentException($"Could not get a string from console");
-                }
+                if (null == entered) throw new ArgumentException("Could not get a string from console");
+
                 var value = Convert.ChangeType(entered, paramType);
                 if (null == value)
-                {
                     throw new ArgumentException(
                         $"Could not convert provided string '{entered}' to value of type {paramType}");
-                }
 
                 list.Add(value);
             }
@@ -73,17 +60,14 @@ namespace Demo
             var ctorInfosLength = ctorInfos.Length;
             while (true)
             {
-                for (int i = 0; i < ctorInfosLength; i++)
+                for (var i = 0; i < ctorInfosLength; i++)
                 {
                     var ctorInfo = ctorInfos[i];
                     var parameterInfos = ctorInfo.GetParameters();
                     var sb = new StringBuilder();
-                    foreach (var param in parameterInfos)
-                    {
-                        sb.Append($"{param.ParameterType} {param.Name}, ");
-                    }
+                    foreach (var param in parameterInfos) sb.Append($"{param.ParameterType} {param.Name}, ");
 
-                    Console.WriteLine($"{i + 1} {sb.ToString().TrimEnd(new char[] {',', ' '})}");
+                    Console.WriteLine($"{i + 1} {sb.ToString().TrimEnd(',', ' ')}");
                 }
 
                 Console.WriteLine("Which one you choose?");
@@ -96,25 +80,6 @@ namespace Demo
                 {
                     // ignored
                 }
-            }
-        }
-
-        private static IShape InitializeShapeWithArguments(string shapeFullName, object[] ctorArgs)
-        {
-            var shapeType = Type.GetType(shapeFullName);
-            if (null == shapeType) throw new ArgumentException($"Could not determine the type of '{shapeFullName}'");
-
-            try
-            {
-                var shape = Activator.CreateInstance(shapeType, ctorArgs);
-                if (null == shape) throw new Exception($"Could not instantiate and object of type '{shapeFullName}'");
-
-                return (IShape) shape;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
             }
         }
 
